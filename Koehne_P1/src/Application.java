3f746f9537d3722ab204f38ea861d33055103ca5
unknown <thomaskoehnej@gmail.com>
2020-01-30 10:02:53 -0500

    added Project Files

---
+++ Koehne_P1/src/Application.java
@@ -0,0 +1,11 @@
+//Thomas Koehne 1/28/20
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Koehne_P2/src/BMICalculator.java
@@ -0,0 +1,5 @@
+//Thomas Koehne 1/28/20
+
+public class BMICalculator {
+
+}
